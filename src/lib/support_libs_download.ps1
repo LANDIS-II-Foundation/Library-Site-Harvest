525e886292adfd48b3c52b08f09bfb11b4791e61
Melissa Lucash <mslucash@gmail.com>
2026-04-16 10:05:23 -0700

    needed to reference succession v10, not 9

--- src/lib/support_libs_download.ps1
+++ src/lib/support_libs_download.ps1
@@ -1,18 +1,20 @@
 # LANDIS-II support library GitHub URL
-# $master = "https://github.com/LANDIS-II-Foundation/Support-Library-Dlls-v8/raw/main/"
-$master = "https://github.com/rmscheller/Support-Library-Dlls-v8/raw/main/"
+$master = "https://github.com/LANDIS-II-Foundation/Support-Library-Dlls-v8/raw/main/"
+
 
 #************************************************
 # LANDIS-II support library dependencies
 # Modify here when any dependencies changed 
 
-$dlls = "Landis.Library.Succession-v9.dll",
-"Landis.Library.UniversalCohorts-v2.dll"
+$dlls = "Landis.Library.UniversalCohorts-v2.dll",
+"Landis.Library.Succession-v10.dll"
+
 #************************************************
 
 
 # LANDIS-II support libraries download
 $current = Get-Location
+write $current
 $outpath = $current.toString() + "/"
 
 try {
